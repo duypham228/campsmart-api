6a848de91a10986a6b9ea13dc5d7c0d379025096
duypham228 <duy.cs.pham@gmail.com>
2021-07-15 15:28:01 -0500

    fix

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ def homepage():
 
 @app.route("/<location>")
 def weather(location):
-	#Parameters for the API to find the location ID
-    
+    #Parameters for the API to find the location ID
+
     #Request the API to get the location id
 
     #Get the id from the response
- 
+
     #Request the API to get the weather based on location id
-    
+
     #converting to JSON
-    
+
     #Get the consolidated weather from the JSON file
-    
+
     #Get the list of weather info for today weather
 
-	#Get today temperature from the list
+    #Get today temperature from the list
     
 	return render_template('weather.html', location=location, temp=today_temp)
 
